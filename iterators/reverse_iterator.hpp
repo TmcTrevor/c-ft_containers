3eba7e12b512ec52524e40805d05cced2db24ec7
Mohamed Khames <mokhames@e2r12p12.1337.ma>
2022-03-09 00:57:05 +0100

    lp

--- iterators/reverse_iterator.hpp
+++ iterators/reverse_iterator.hpp
@@ -19,24 +19,86 @@ namespace ft {
 
         public :
             ~reverse_iterator();
+            
             reverse_iterator() { rv = NULL; }
+            
             reverse_iterator(iterator_type rv) { this->rv = rv; }
+            
             reverse_iterator(reverse_iterator const &c) { rv = c.rv; }
+            
             template<typename iterator1>
             reverse_iterator operator=(reverse_iterator<iterator1> const &c) { rv = c.rv; return *this; }
+            
             iterator_type base() const { return rv; }
+            
             reference operator*() const 
             {
-                return (*r);
+                return (*--rv);
             }
+
             pointer operator->() const
             {
                 return (rv);
             }
+
             Ra_iterator &operator++()
             {
-                
+                --rv;
+                return (*this++);
             }
+
+            Ra_iterator &operator++()
+            {
+                 ++rv;
+                return *this;
+            }
+
+            Ra_iterator operator--(int)
+            {
+                Ra_iterator copy(*this);
+                ++rv;
+                return copie;
+            }
+
+            Ra_iterator operator++(int)
+            {   
+                Ra_iterator copy(*this);
+                --rv;
+                return copy;
+            }
+
+        /** ************************************************************************** */
+		/**                                ARETHMETIC OPERATORS                        */
+		/** ************************************************************************** */
+
+
+        Ra_iterator operator+(difference_type n)
+        {
+            return (this->rv - n);
+        }
+        
+        Ra_iterator operator-(difference_type n)
+        {
+            return (this->rv + n);
+        }
+
+        Ra_iterator operator+=(difference_type n)
+        {
+            rv -= n;
+            return (*this);
+        }
+
+        Ra_iterator operator-=(difference_type n)
+        {
+            rv += n;
+            return (*this);
+        }
+
+        Ra_iterator operator[](difference_type n)
+        {
+            return (*(this->it));
+        }
+
 
 
 
--- iterators/test.cpp
+++ iterators/test.cpp
@@ -1,63 +1,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class operation
-{
-    int a,b,add,sub,mul;
-    float div;
-public:
-    void get();
-    void sum();
-    void difference();
-    void product();
-    void division();
-};
-inline void operation :: get()
-{
-    cout << "Enter first value:";
-    cin >> a;
-    cout << "Enter second value:";
-    cin >> b;
-}
-  
-inline void operation :: sum()
-{
-    add = a+b;
-    cout << "Addition of two numbers: " << a+b << "\n";
-}
-  
-inline void operation :: difference()
-{
-    sub = a-b;
-    cout << "Difference of two numbers: " << a-b << "\n";
-}
-  
-inline void operation :: product()
-{
-    mul = a*b;
-    cout << "Product of two numbers: " << a*b << "\n";
-}
-  
-inline void operation ::division()
-{
-    div=a/b;
-    cout<<"Division of two numbers: "<<a/b<<"\n" ;
-}
+
   
 int main()
 {
-    cout << "Program using inline function\n";
-    operation s;
-    s.get();
-    s.sum();
-    s.difference();
-    s.product();
-    s.division();
     vector<int> a;
-    a.push_back(10);
-    vector<int>::iterator c;
 
-    c.base();
-
+    cout << a.capacity() << endl << a.size() << endl << a.max_size() << endl;
     return 0;
 }
